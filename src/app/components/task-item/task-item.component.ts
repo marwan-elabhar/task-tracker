a917d75a91e8364698d45bcadb9a81968df2622f
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-01-27 19:55:54 +0200

    emit deleteTask and toggleReminder methods from task-item

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,5 @@
-import { Component, Input } from '@angular/core';
-import { NgStyle } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { NgStyle, NgClass } from '@angular/common';
 import { Task } from '../../Task';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
 import { FaIconComponent } from '@fortawesome/angular-fontawesome';
@@ -7,7 +7,7 @@ import { FaIconComponent } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [FaIconComponent, NgStyle],
+  imports: [FaIconComponent, NgStyle, NgClass],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.scss'
 })
@@ -15,4 +15,17 @@ export class TaskItemComponent {
   @Input()
   task!: Task;
   faTimes = faTimes
+
+  @Output() onDeleteTask: EventEmitter<Task> = new EventEmitter()
+  @Output() onToggleReminder: EventEmitter<Task> = new EventEmitter()
+
+
+  onDelete(task:Task) {
+    this.onDeleteTask.emit(task)
+  }
+
+  onToggle(task:Task) {
+    this.onToggleReminder.emit(task)
+
+  }
 }
