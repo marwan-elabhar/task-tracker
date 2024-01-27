50f34d09bbd80f257706b7386b40bf05b8717b49
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-01-27 21:18:41 +0200

    remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AboutComponent } from './components/pages/about/about.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
