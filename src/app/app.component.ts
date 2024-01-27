49fce0138885b3623329a681bb0e34bf2859ed0d
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-01-27 21:12:06 +0200

    add footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { TasksComponent } from './components/tasks/tasks.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, TasksComponent],
+  imports: [RouterOutlet, HeaderComponent, TasksComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
