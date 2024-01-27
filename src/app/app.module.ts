9f4f18aa7bb50be5d61f5603512b6481bc9bc349
marwan-elabhar <marwan.a.elabhar@gmail.com>
2024-01-27 21:11:46 +0200

    add about page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { AboutComponent } from './components/pages/about/about.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
